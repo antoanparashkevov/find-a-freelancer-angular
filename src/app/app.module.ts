9d197c50fb583ec4896c6adcfea3f69a3b672289
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-05 15:26:40 +0200

    Register routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes} from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FreelancersListComponent } from './features/freelancer/pages/freelancers-list/freelancers-list.component'
+import { FreelancerDetailsComponent } from './features/freelancer/pages/freelancer-details/freelancer-details.component'
+import { ProposalsReceivedComponent } from './features/proposal/pages/proposals-received/proposals-received.component'
+import { FreelancerRegistrationComponent } from './features/freelancer/pages/freelancer-registration/freelancer-registration.component'
+
+const routes: Routes = [
+    { path: '', redirectTo: '/freelancers' },
+    { path: 'freelancers', component: FreelancersListComponent },
+    { path: 'freelancers/:id', component: FreelancerDetailsComponent },
+    { path: 'proposals', component: ProposalsReceivedComponent },
+    { path: 'register', component: FreelancerRegistrationComponent },
+    // {path: 'auth', component},
+    // {path: ''}//notFound page
+]
 
 @NgModule({
   declarations: [
