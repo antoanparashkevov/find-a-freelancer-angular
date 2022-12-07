c24ef351f7e330869138f503c34a6f22d8434435
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-07 19:36:07 +0200

    Added a Contact form + validation

--- src/app/features/freelancer/pages/freelancer-contact-form/freelancer-contact-form.component.ts
+++ src/app/features/freelancer/pages/freelancer-contact-form/freelancer-contact-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm, NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-freelancer-contact-form',
@@ -6,10 +7,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./freelancer-contact-form.component.scss']
 })
 export class FreelancerContactFormComponent implements OnInit {
+    formIsValid: boolean = true;
+    @ViewChild('formRef') formRef!: NgForm
+    
+    constructor() { }
+    
+    ngOnInit(): void {
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    onSubmit(formRef: NgForm, email: NgModel, message: NgModel) {
+        console.log('Form >>> ', this.formRef)
+        console.log('Email >>> ', email)
+        console.log('Message >>> ', message)
+        if(formRef.submitted && formRef.form.invalid) {
+            this.formIsValid = false
+        } else {
+            this.formIsValid = true;
+        }
+    }
+    
+    clearInvalidClass(inputType: string) {
+    
+    }
 }
+// name: 'ContactCoach',
+//         data() {
+//             return {
+//                 message: {
+//                     val: '',
+//                     isValid: true
+//                 },
+//                 email: {
+//                     val: '',
+//                     isValid: true
+//                 },
+//                 formIsValid: true
+//             };
+//         },
+//         methods: {
+//             clearInvalidClass(input) {
+//                 if(this[input].val !== ''){
+//                     this[input].isValid = true;
+//                 }
+//             },
+//             validateForm() {
+//                 if (this.email.val === '' || this.email.val.includes('@') === false) {
+//                     this.email.isValid = false;
+//                     this.formIsValid = false;
+//                 }
+//                 if (this.message.val === '') {
+//                     this.message.isValid = false;
+//                     this.formIsValid = false;
+//                 }
+//             },
+//             submitForm() {
+//                 this.formIsValid = true;
+//                 this.validateForm();
+//                 if (this.formIsValid === false) {
+//                     return;
+//                 }
+//                 const formData = {
+//                     coachId: this.$route.params.id,
+//                     email: this.email.val,
+//                     message: this.message.val
+//                 };
+//                 this.$store.dispatch('requests/addRequest', formData);
+//                 this.$router.replace('/coaches');
+//             }
+//         }
+//     };
