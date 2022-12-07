67efb7fc8090d9eda2feba11f242e2a65d2263d0
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-07 21:18:48 +0200

    dynamically catch the checkbox inputs for filtering the freelancers

--- src/app/features/freelancer/components/freelancer-filter/freelancer-filter.component.ts
+++ src/app/features/freelancer/components/freelancer-filter/freelancer-filter.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-freelancer-filter',
@@ -6,17 +7,35 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./freelancer-filter.component.scss']
 })
 export class FreelancerFilterComponent implements OnInit {
-        skills: string[] = [
-            'type1',
-            'type2',
-            'type3',
-            'type4',
-            'type5',
-        ]
+        skills: {[id: string] : boolean} = {
+                type1: true,
+                type2: true,
+                type3: true,
+                type4: true,
+                type5: true,
+            }
     
         constructor() { }
         
         ngOnInit(): void {
+        }
+        
+        setFilter(ev: Event) {
+            //the change event is triggered whenever the checked property for example changes from true to false and from false to true
+            
+            //TODO to remove logs
+            console.log('triggered!')
+            const target = ev.target as HTMLInputElement
+            const filterId = target.id;
+            console.log('id >>', target.id)
+            const isChecked = target.checked
+            console.log('is checked >>> ',target.checked)
+            
+            const updatedFilter = {
+                ...this.skills,
+                [filterId]: isChecked
+            }
+            console.log(updatedFilter)
         }
 
 }
