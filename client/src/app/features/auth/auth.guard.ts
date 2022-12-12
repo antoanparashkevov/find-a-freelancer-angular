0d8715d87204dd2d55f528e40d645317751ee8e9
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-12 20:08:35 +0200

    some additions to the auth guard

--- client/src/app/features/auth/auth.guard.ts
+++ client/src/app/features/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
 import {Injectable} from "@angular/core";
 import {Observable} from 'rxjs';
-import {map} from "rxjs/operators";
+import {map, take} from "rxjs/operators";
 import {AuthService} from "./services/auth.service";
 
 @Injectable({ providedIn: 'root' })
@@ -11,7 +11,9 @@ export class AuthGuard implements CanActivate {
     }
     
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        return this.authService.user.pipe(map(user => {
+        return this.authService.user.pipe(
+            take(1),//this subject can emit data more than once, with this operator we will listen to the last emitted data
+            map(user => {
             const isAuth =  !!user;//to convert it to boolean
             if(isAuth) {
                 return true;
