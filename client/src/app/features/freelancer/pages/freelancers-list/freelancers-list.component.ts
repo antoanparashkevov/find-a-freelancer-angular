d2294a3ba740ac7e7f7ba82e355374dc7ff14af2
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-13 20:15:08 +0200

    Conditionally add Join Us button and register button

--- client/src/app/features/freelancer/pages/freelancers-list/freelancers-list.component.ts
+++ client/src/app/features/freelancer/pages/freelancers-list/freelancers-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Freelancer } from "../../models/freelancer.model";
 import {FreelancerService} from "../../services/freelancer.service";
 import {FreelancerStorage} from "../../services/freelancer-storage.service";
+import {AuthService} from "../../../auth/services/auth.service";
 
 @Component({
   selector: 'app-freelancers-list',
@@ -10,7 +11,7 @@ import {FreelancerStorage} from "../../services/freelancer-storage.service";
 })
 export class FreelancersListComponent implements OnInit {
     freelancers: Freelancer[] = []
-    
+    isAuthenticated: boolean = false
     skills: {[id: string] : boolean} = {
         type1: true,
         type2: true,
@@ -21,13 +22,17 @@ export class FreelancersListComponent implements OnInit {
     
     constructor(
         private freelancerService: FreelancerService,
-        private freelancerStorage: FreelancerStorage
+        private freelancerStorage: FreelancerStorage,
+        private authService: AuthService
     ) { }
 
     ngOnInit(): void {
         this.freelancerStorage.fetchFreelancers().subscribe(res=>{
             this.freelancers = res
             console.log('Freelancers from the Service >>> ', this.freelancers)
+        })
+        this.authService.user.subscribe((user) => {
+            this.isAuthenticated = !!user;//if it has a user data, to return boolean, not the actual user data
         })
     }
     
