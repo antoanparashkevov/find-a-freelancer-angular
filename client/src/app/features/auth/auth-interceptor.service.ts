7739ad95e9de959371b223e30cdaa0b0dbeac4cd
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-16 09:51:40 +0200

    some modifications to the interceptor (always set x-authorization header)

--- client/src/app/features/auth/auth-interceptor.service.ts
+++ client/src/app/features/auth/auth-interceptor.service.ts
@@ -5,25 +5,24 @@ import * as module from "module";
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         
-        if( !req.url.includes('http://localhost:3030/freelancersData/freelancers') || (req.url.includes('http://localhost:3030/freelancersData/freelancers') && req.method === 'POST')  ) {
-           
             console.log('Request URL  from the interceptor >>> ', req.url)
             console.log('Request Method >>> ', req.method)
             
-            let token  = localStorage.getItem('authToken')
-            
-            if(token && token.includes('"')) {
-                //cut the quotes
-                token = token.slice(1, token.length - 1)
+            if(localStorage.length > 0) {
+                let token  = localStorage.getItem('authToken')
+
+                if(token && token.includes('"')) {
+                    //cut the quotes
+                    token = token.slice(1, token.length - 1)
+                }
+
+                console.log('Token from Auth-Interceptor >>> ', token)
+                if(token !== null) {
+                    const modifiedRequest = req.clone({headers: req.headers.append('x-authorization', token)})
+                    return next.handle(modifiedRequest);
+                }
             }
             
-            console.log('Token from Auth-Interceptor >>> ', token)
-           
-            if(token !== null) {
-                const modifiedRequest = req.clone({headers: req.headers.append('x-authorization', token)})
-                return next.handle(modifiedRequest);
-            }
-        }
         return next.handle(req)
     }
 }
