3ca8ea0b81029e1d5e793f7f35fb881a9531ed8d
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-18 11:13:18 +0200

    add a disabled style for .edit and .delete button classes

--- client/src/app/features/profile/services/freelancer-registration-resolver.service.ts
+++ client/src/app/features/profile/services/freelancer-registration-resolver.service.ts
@@ -1,9 +1,9 @@
-import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
 import {Freelancer} from "../../freelancer/models/freelancer.model";
 import {Observable} from "rxjs";
 import {FreelancerStorage} from "../../freelancer/services/freelancer-storage.service";
 import {Injectable} from "@angular/core";
-import {map} from "rxjs/operators";
+import {map, tap} from "rxjs/operators";
 
 @Injectable()
 export class FreelancerRegistrationResolver implements Resolve<Freelancer> {
@@ -12,6 +12,7 @@ export class FreelancerRegistrationResolver implements Resolve<Freelancer> {
     
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Freelancer> | Promise<Freelancer> | Freelancer  {
         let id = route.params['id'];
+        //or with tap operator
         return this.freelancerStorage.getIndividualFreelancer(id).pipe(map(
             (data: Freelancer)=>{
                     return data
