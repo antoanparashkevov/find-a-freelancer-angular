a5217ee21f812ddba9451ecbe3f863c268fb9c19
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-27 11:52:27 +0200

    enhance the type of actions to avoid clashes

--- client/src/app/features/freelancer/store/freelancers.actions.ts
+++ client/src/app/features/freelancer/store/freelancers.actions.ts
@@ -1,9 +1,9 @@
 import {Action} from "@ngrx/store";
 import {Freelancer} from "../models/freelancer.model";
 
-export const STORE_FREELANCER: string = 'STORE_FREELANCER'
-export const EDIT_FREELANCER: string = 'EDIT_FREELANCER'
-export const DELETE_FREELANCER: string = 'DELETE_FREELANCER'
+export const STORE_FREELANCER: string = '[Freelancers] Store_Freelancer'
+export const EDIT_FREELANCER: string = '[Freelancers] Edit_Freelancer'
+export const DELETE_FREELANCER: string = '[Freelancers] Delete_Freelancer'
 
 //!!! the Action interface forces us only to use type property, not payload. !!!
 export class StoreFreelancer implements Action {
