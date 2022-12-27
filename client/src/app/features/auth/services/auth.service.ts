0183b9578ee1fdb3eb42b03e1b6f2cb5bd1c430a
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-27 10:45:05 +0200

    dispatching auth actions inside auth service

--- client/src/app/features/auth/services/auth.service.ts
+++ client/src/app/features/auth/services/auth.service.ts
@@ -4,7 +4,9 @@ import {Router} from "@angular/router";
 import { catchError, tap } from 'rxjs/operators';
 import {BehaviorSubject, Subject} from "rxjs";
 import {User} from "../models/user.model";
-
+import {Store} from "@ngrx/store";
+import * as fromApp from '../../../store/app.reducer'
+import * as AuthActions from "../store/auth.actions";
 export interface AuthResponseData {
     accessToken: string;
     email: string;
@@ -18,7 +20,11 @@ export class AuthService {
   //the Subject is like a custom event emitter. It is more recommended
   user = new BehaviorSubject<User | null>(null)
     
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(
+      private http: HttpClient, 
+      private router: Router,
+      private store: Store<fromApp.GlobalAppState>
+      ) { }
 
     register(email: string, password: string) {
       //will return an observable
@@ -55,7 +61,12 @@ export class AuthService {
     
     private handleAuthentication(email: string, _id: string, token: string) {
         const user = new User(email, _id, token)
-        this.user.next(user)//emit this user
+        this.user.next(user)//emit this user, besides Subjects, we will use ngRx
+        this.store.dispatch(new AuthActions.Login({
+            email: user.email,
+            id: user._id,
+            token: user.token
+        }))
         //converts to JSON string object
         localStorage.setItem('email', JSON.stringify(email))
         localStorage.setItem('authToken', JSON.stringify(user.token))
@@ -75,7 +86,13 @@ export class AuthService {
                 const loadedUser = new User(email, userId, authToken)
                 
                 if(loadedUser.token) {
-                  this.user.next(loadedUser)
+                  this.user.next(loadedUser)//besides Subjects, we will use ngRx
+                    this.store.dispatch(new AuthActions.Login(
+                        {
+                            email: loadedUser.email,
+                            id: loadedUser._id,
+                            token: loadedUser.token
+                        }))
                 }
             }
       }
@@ -87,8 +104,8 @@ export class AuthService {
             console.log('Response from logging out >>> ', res)
         })
         
-        this.user.next(null)
-        
+        this.user.next(null)//besides Subjects, we will use ngRx
+        this.store.dispatch(new AuthActions.Logout(null))
         this.router.navigate(['/auth'])
         
         localStorage.removeItem('email')
